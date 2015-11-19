be94e853ea132bf9c5b035ab6606305251986b2b
SAC-CS112-Ngo-Son <frank.ngo.1702@gmail.com>
2015-11-18 21:45:45 -0800

    commit

--- src/Test2.java
+++ src/Test2.java
@@ -10,47 +10,48 @@ public class Test2
 	{
 		int iDices, iRound = 0, iScore = 0, iWin = 0, iLose = 0, iChoice;
 		char cResult;
-		char[] aryGuess = new char[100]; //array to store user's guesses
-		int[] aryResult = new int[100]; //array to store result for each round
+		char[] aryGuess = new char[100]; // array to store user's guesses
+		int[] aryResult = new int[100]; // array to store result for each round
 
 		iDices = Integer.parseInt(JOptionPane.showInputDialog("Enter the number of dices: "));
 		Low_High game = new Low_High(iDices);
 
 		do
 		{
-			iRound++; //count the number of times played
-			
-			//ask for user's guess, making sure the input is correct
+			iRound++; // count the number of times played
+
+			// ask for user's guess, making sure the input is correct
 			do
 			{
-			aryGuess[iRound] = (JOptionPane
-					.showInputDialog(String.format("Round %d: What's your guess? (L for Low and H for High)", iRound)))
-							.charAt(0);
+				aryGuess[iRound] = (JOptionPane.showInputDialog(
+						String.format("Round %d: What's your guess? (L for Low and H for High)", iRound))).charAt(0);
 			} while ((aryGuess[iRound] != 'L') && (aryGuess[iRound] != 'H'));
-			
+
 			cResult = game.Low_Or_High();
-			
-			System.out.println(cResult); //tracking and matching results
-			
-			if (aryGuess[iRound] == cResult) //win case
+
+			System.out.println(cResult); // tracking and matching results
+
+			if (aryGuess[iRound] == cResult) // win case
 			{
-				aryResult[iRound] = 1; //1 for correct guess
-				iScore++; //increase score by 1
-				iWin++; //increase the number of wins by 1
+				aryResult[iRound] = 1; // 1 for correct guess
+				iScore++; // increase score by 1
+				iWin++; // increase the number of wins by 1
 				iChoice = JOptionPane.showConfirmDialog(null, "Your guess is correct. Do you want to continue?", null,
 						JOptionPane.YES_NO_OPTION);
-			} else //lose case
+			} else // lose case
 			{
-				aryResult[iRound] = 0; //0 for incorrect guess
-				iScore--; //decrease score by 1
-				iLose++; //increase the number of loses by 1
+				aryResult[iRound] = 0; // 0 for incorrect guess
+				iScore--; // decrease score by 1
+				iLose++; // increase the number of loses by 1
 				iChoice = JOptionPane.showConfirmDialog(null, "Your guess is incorrect. Do you want to continue?", null,
 						JOptionPane.YES_NO_OPTION);
 			}
-		} while (iChoice == JOptionPane.YES_OPTION); //keep playing if the user chooses YES
+		} while (iChoice == JOptionPane.YES_OPTION); // keep playing if the user
+														// chooses YES
 
 		JOptionPane.showMessageDialog(null,
-				String.format("You've played %d times.\nYou've won %d times and lost %d times.\nYour final score is %d.",
+				String.format(
+						"You've played %d times.\nYou've won %d times and lost %d times.\nYour final score is %d.",
 						iRound, iWin, iLose, iScore));
 	}
 
